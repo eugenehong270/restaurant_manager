7a8a3f745c887e57a8f7a5f39364b1559351274c
Eugene Hong <ehong270@gmail.com>
2023-05-01 19:51:53 -0400

    warehouse.js file

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainPage from './MainPage';
 
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+
+        <Route />
+
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- react_app/src/MainPage.js
+++ react_app/src/MainPage.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainPage() {
   return (
-    <div>Hi</div>
+    <div>
+       Main page
+    </div>
   )
 }
 
