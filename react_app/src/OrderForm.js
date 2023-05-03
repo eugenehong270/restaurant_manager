0d42e2f20bc3f893aecbb8b4fe64ce764d546b46
Eugene Hong <ehong270@gmail.com>
2023-05-03 17:26:04 -0400

    order form progress

--- react_app/src/OrderForm.js
+++ react_app/src/OrderForm.js
@@ -1,86 +1,108 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Select from 'react-select';
 
 function OrderForm() {
   const [restaurants, setRestaurants] = useState([]);
-  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-  const [inventoryItems, setInventoryItems] = useState([]);
-  const [selectedInventoryItem, setSelectedInventoryItem] = useState(null);
+  const [selectedRestaurant, setSelectedRestaurant] = useState('');
+  const [inventory, setInventory] = useState([]);
+  const [selectedItem, setSelectedItem] = useState('');
   const [quantity, setQuantity] = useState('');
-  const [warehouses, setWarehouses] = useState([]);
-  const [message, setMessage] = useState('');
+  const [closestWarehouses, setClosestWarehouses] = useState([]);
 
   useEffect(() => {
-    async function fetchRestaurants() {
-      try {
-        const response = await axios.get('http://localhost:8000/api/restaurants/');
-        setRestaurants(response.data);
-      } catch (error) {
-        setMessage('Error: Could not retrieve restaurants');
-      }
+    async function fetchData() {
+      const response = await axios.get('http://localhost:8000/api/restaurants/');
+      setRestaurants(response.data);
     }
-    fetchRestaurants();
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    async function fetchInventoryItems() {
-      try {
-        const response = await axios.get('http://localhost:8000/api/inventory/');
-        setInventoryItems(response.data);
-      } catch (error) {
-        setMessage('Error: Could not retrieve inventory items');
+  async function fetchInventory() {
+    const response = await axios.get('http://localhost:8000/api/inventory/');
+    const inventory = response.data;
+    const inventoryGrouped = {};
+    inventory.forEach(item => {
+      if (inventoryGrouped[item.sku]) {
+        inventoryGrouped[item.sku].quantity += item.quantity;
+      } else {
+        inventoryGrouped[item.sku] = {
+          ...item,
+        };
       }
-    }
-    fetchInventoryItems();
+    });
+    const inventoryList = Object.values(inventoryGrouped);
+    setInventory(inventoryList);
+  }
+
+
+  useEffect(() => {
+    fetchInventory();
   }, []);
 
   async function handleSubmit(event) {
     event.preventDefault();
     try {
-      const response = await axios.get(`http://localhost:8000/api/warehouses/?item_name=${selectedInventoryItem.label}&quantity__gte=${quantity}&restaurant=${selectedRestaurant.value}`);
-      setWarehouses(response.data);
-      setMessage('');
+      const warehousesResponse = await axios.get('http://localhost:8000/api/warehouses/');
+      const warehouseIds = warehousesResponse.data.map(warehouse => warehouse.id);
+
+      const closestWarehouses = [];
+      for (let i = 0; i < warehouseIds.length; i++) {
+        const inventoryResponse = await axios.get(`http://localhost:8000/api/inventory/?warehouse=${warehouseIds[i]}`);
+        const chosenItemInventory = inventoryResponse.data.filter(item => item.sku === selectedItem && item.quantity >= quantity);
+        if (chosenItemInventory.length > 0) {
+          const warehouseResponse = await axios.get(`http://localhost:8000/api/warehouses/${warehouseIds[i]}/`);
+          closestWarehouses.push({
+            ...warehouseResponse.data,
+            quantity: chosenItemInventory[0].quantity,
+            sku: chosenItemInventory[0].sku,
+            item_name: chosenItemInventory[0].item_name,
+          });
+        }
+      }
+      setClosestWarehouses(closestWarehouses);
     } catch (error) {
-      setMessage('Error: Could not retrieve warehouses');
+      console.error(error);
     }
   }
 
-  function renderOptions(options) {
-    return options.map((option) => ({
-      value: option.id,
-      label: option.name,
-    }));
+  function handleRestaurantChange(event) {
+    setSelectedRestaurant(event.target.value);
   }
 
-  function renderInventoryOptions(options) {
-    return options.map((option) => ({
-      value: option.id,
-      label: option.item_name,
-    }));
+  function handleItemChange(event) {
+    setSelectedItem(event.target.value);
+  }
+
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
   }
 
   return (
     <div>
-      <h1>Place Order</h1>
+      <h1>Order Form</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="restaurant">Restaurant:</label>
-          <Select
-            id="restaurant"
-            value={selectedRestaurant}
-            options={renderOptions(restaurants)}
-            onChange={(option) => setSelectedRestaurant(option)}
-          />
+          <select id="restaurant" value={selectedRestaurant} onChange={handleRestaurantChange}>
+            <option value="">Select a restaurant</option>
+            {restaurants.map(restaurant => (
+              <option key={restaurant.id} value={restaurant.id}>
+                {restaurant.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
-          <label htmlFor="inventory-item">Inventory Item:</label>
-          <Select
-            id="inventory-item"
-            value={selectedInventoryItem}
-            options={renderInventoryOptions(inventoryItems)}
-            onChange={(option) => setSelectedInventoryItem(option)}
-          />
+          <label htmlFor="item">Item:</label>
+          <select id="item" value={selectedItem} onChange={handleItemChange}>
+  <option value="">Select an item</option>
+  {inventory.map(item => (
+    <option key={item.sku} value={item.sku}>
+      SKU: {item.sku} - {item.item_name}
+    </option>
+  ))}
+</select>
+
         </div>
         <div>
           <label htmlFor="quantity">Quantity:</label>
@@ -88,26 +110,25 @@ function OrderForm() {
             type="number"
             id="quantity"
             value={quantity}
-            onChange={(event) => setQuantity(event.target.value)}
+            onChange={handleQuantityChange}
           />
         </div>
-        <button type="submit">Find Warehouses</button>
+        <button type="submit">Submit</button>
       </form>
-      {message && <p>{message}</p>}
-      {warehouses.length > 0 && (
-        <div>
-          <h2>Closest Warehouses:</h2>
-          <ul>
-            {warehouses.map((warehouse) => (
-              <li key={warehouse.id}>
-                {warehouse.name} ({warehouse.distance} km)
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+      {closestWarehouses.length > 0 && (
+  <div>
+    <h2>Closest Warehouses:</h2>
+    <ul>
+      {closestWarehouses.map(warehouse => (
+        <li key={warehouse.id}>
+          {warehouse.name} - Quantity: {warehouse.quantity}, SKU: {warehouse.sku}, Item Name: {warehouse.item_name}
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
+
+</div>
   );
 }
-
-export default OrderForm;
+export default OrderForm
--- warehouses/models.py
+++ warehouses/models.py
@@ -8,8 +8,6 @@ class Warehouse(models.Model):
     def __str__(self):
         return self.name
 
-from django.db import models
-
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     x_coordinate = models.FloatField()
